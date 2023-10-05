8124c032b7786f4fdb2a02d80304d45734a8e24e
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2023-10-05 18:28:56 +0300

    init

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const { default: dts } = require('rollup-plugin-dts');
+
+const packageJson = require('./package.json');
+
+module.exports = [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json', sourceMap: false }),
+    ]
+  },
+  {
+    input: 'dist/mwa-logger/esm/types/index.d.ts',
+    output: [
+      { file: 'dist/mwa-logger/index.d.ts', format: 'esm' }
+    ],
+    plugins: [dts()]
+  }
+]
