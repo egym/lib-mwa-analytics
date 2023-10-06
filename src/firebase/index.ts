3850b19877a68f5d8ec3418b05f617d482425145
alexanderkolomiytsev <alexander.kolomiytsev@gmail.com>
2023-10-06 04:54:47 +0300

    remove custom gtag name

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,7 +4,6 @@ import { Analytics, settings, getAnalytics, logEvent } from 'firebase/analytics'
 const CUSTOM_HOSTED_GTAG_URL = '/mwa-analytics/gtag.js';
 const GTAG_URL = 'https://www.googletagmanager.com/gtag/js';
 const FirebaseDataLayerName = 'dataLayerCustomMWATracking';
-const FirebaseGtagName = 'gtagCustomMWATracking';
 
 let app: FirebaseApp | null = null;
 let analytics: Analytics | null = null;
@@ -22,7 +21,6 @@ const loadAnalyticsScript = (firebaseConfig: FirebaseOptions) => {
 const initAnalytics = (firebaseApp: FirebaseApp) => {
   settings({
     dataLayerName: FirebaseDataLayerName,
-    gtagName: FirebaseGtagName,
   });
   analytics = getAnalytics(firebaseApp);
   return analytics;
